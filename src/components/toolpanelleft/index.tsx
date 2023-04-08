9df4b34d0ae7d86bd8ffaa7332698c04b8d97967
KostyaCube <psychocube91@gmail.com>
2023-04-08 17:24:46 +0600

    leftpanel changed

--- src/components/toolpanelleft/index.tsx
+++ src/components/toolpanelleft/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import IconButton from '@mui/material/IconButton';
-import { Ruler } from 'assets/icons/toolPanelLeft/ruler';
-import { Annotation } from 'assets/icons/toolPanelLeft/annotation';
-import { Ruler2 } from 'assets/icons/toolPanelLeft/ruler2';
-import { Arrow } from 'assets/icons/toolPanelLeft/arrow';
+import { Ruler } from 'assets/iconsButtons/toolPanelLeft/ruler';
+import { Annotation } from 'assets/iconsButtons/toolPanelLeft/annotation';
+import { Ruler2 } from 'assets/iconsButtons/toolPanelLeft/ruler2';
+import { Arrow } from 'assets/iconsButtons/toolPanelLeft/arrow';
 import { Wrapper } from './styles';
 import Draggable from 'react-draggable';
 
@@ -11,40 +10,14 @@ export default function ToolPanelLeft(): JSX.Element {
   return (
     <Draggable>
       <Wrapper>
-        <IconButton
-          sx={{
-            
-          }}>
-          <Ruler />
-        </IconButton>
-
-        <IconButton>
-          <Annotation />
-        </IconButton>
-
-        <IconButton>
-          <Arrow />
-        </IconButton>
-
-        <IconButton>
-          <Arrow />
-        </IconButton>
-
-        <IconButton>
-          <Annotation />
-        </IconButton>
-
-        <IconButton>
-          <Ruler2 />
-        </IconButton>
-
-        <IconButton>
-          <Arrow />
-        </IconButton>
-
-        <IconButton>
-          <Arrow />
-        </IconButton>
+        <Ruler />
+        <Annotation />
+        <Arrow />
+        <Arrow />
+        <Annotation />  
+        <Ruler2 />
+        <Arrow />
+        <Arrow />
       </Wrapper>
     </Draggable>
   );
--- src/components/toolpanelleft/styles.ts
+++ src/components/toolpanelleft/styles.ts
@@ -4,7 +4,32 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   max-width: 42px;
+  padding: 2px;
   &:hover{
     cursor: grabbing;
   }
+`;
+
+export const IconButtonWrapper = styled.button`
+background-color: #FFF;
+margin: 2px;
+border-radius: 4px;
+border: none;
+padding: 1px 0 0 0;
+width: 28px;
+&:hover {
+  cursor: pointer;
+  background-color: #E9E9EB;
+  /* svg {   
+      path {
+        fill: green;
+      }
+      rect {
+        fill: green;
+      }    
+  } */
+}
+&:active {
+  transform: scale(0.9);
+}
 `;
